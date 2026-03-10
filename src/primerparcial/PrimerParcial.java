4f2804b81ba3c0ab6756e3193132e46259f0ff5e
Melvin <Melvin@MELVIN_LEMUS>
2026-03-09 19:23:28 -0600

    1 Inverso multiplicativo

---
+++ src/primerparcial/PrimerParcial.java
@@ -0,0 +1,13 @@
+
+package primerparcial;
+
+
+public class PrimerParcial {
+
+
+    public static void main(String[] args) {
+        calculadora v = new calculadora();
+        v.setVisible(true);
+    }
+    
+}
