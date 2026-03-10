3eb14749ee151367c5378efab39d7e1568d00efa
Melvin <Melvin@MELVIN_LEMUS>
2026-03-09 20:31:14 -0600

    agregar calculadora

--- src/primerparcial/PrimerParcial.java
+++ src/primerparcial/PrimerParcial.java
@@ -7,6 +7,7 @@ public class PrimerParcial {
 
     public static void main(String[] args) {
         calculadora v = new calculadora();
+        v.setLocationRelativeTo(null);
         v.setVisible(true);
     }
     
